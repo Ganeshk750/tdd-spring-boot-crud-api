b5b88a2424e2969db44cd7b2996731d7c7e52cc5
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-09-29 11:05:40 +0530

    Initial commit

---
+++ src/main/java/com/ganesh/TddSpringBootCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSpringBootCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSpringBootCrudApiApplication.class, args);
+	}
+
+}
